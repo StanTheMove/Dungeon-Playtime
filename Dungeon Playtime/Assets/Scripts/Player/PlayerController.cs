96035b305842e33a4daac34725f071daf50d8409
StanTheMove <stas.kholopkin@gmail.com>
2025-01-22 14:21:50 +0200

    Added Player Controller

---
+++ Dungeon Playtime/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Movement speed")]
+        [SerializeField] private float walkSpeed = 3.0f;
+        [SerializeField] private float sprintMultiplier = 2.0f;
+        [SerializeField] private float crouchSpeed = 1.0f;
+        
+        [Header("Jump Parameters")]
+        [SerializeField] private float jumpForce = 5.0f;
+        [SerializeField] private float gravity = 9.81f;
+        
+        [Header("Camera Parameters")]
+        [SerializeField] private CameraLook cameraLook;
+        
+        private CharacterController characterController;
+        private PlayerInputManager inputManager;
+        private Vector3 currentMovement;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+            inputManager = PlayerInputManager.Instance;
+        }
+
+        private void Update()
+        {
+            if (inputManager == null && cameraLook == null)
+                return;
+            
+            ManageMovement();
+            cameraLook.MouseLook(inputManager.LookInput);
+        }
+
+        void ManageMovement()
+        {
+            if (inputManager == null) return;
+
+            float speed = walkSpeed * (inputManager.SprintValue > 0 ? sprintMultiplier : 1f);
+
+            Vector3 inputDirection = new Vector3(inputManager.MoveInput.x, 0f, inputManager.MoveInput.y);
+            Vector3 worldDirection = transform.TransformDirection(inputDirection);
+            worldDirection.Normalize();
+
+            currentMovement.x = worldDirection.x * speed;
+            currentMovement.z = worldDirection.z * speed;
+
+            ManageJumping();
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
+
+        void ManageJumping()
+        {
+            if (characterController.isGrounded)
+            {
+                currentMovement.y = -0.5f;
+
+                if (inputManager.JumpTriggered)
+                {
+                    currentMovement.y = jumpForce;
+                }
+            }
+            else
+            {
+                currentMovement.y -= gravity * Time.deltaTime;
+            }
+        }
+    }
+}
